8b3ebabad7d2bacc5512f0da3bedc56431e4d054
Anandg01 <ptanandg@gmail.com>
2023-06-28 11:36:49 +0530

    real time Optimising chat

--- public/index.js
+++ public/index.js
@@ -1,15 +1,43 @@
+
 const msgInput = document.getElementById('msginput');
 const token = localStorage.getItem('token')
+const storagemsg = localStorage.getItem('storeMsg');
+const arrst = JSON.parse(storagemsg)
+let lastmsgId = -1
+let bkcontainer = [];
+if (arrst) {
+    const n = arrst.length - 1;
+    bkcontainer=arrst;
+    if (arrst[n]) {
+        createUlList();
+        console.log(arrst)
+        arrst.forEach((el) => {
+            showMsgOnScreen(el.senderName, el.message)
+        })
+        lastmsgId = arrst[n].messageId
+        console.log("this is local stofrage", lastmsgId)
+    }
+}
 
 document.addEventListener('DOMContentLoaded', () => {
     if (!token) {
         window.location.href = '/login.html'
-    } else {
-        axios.get('http://localhost:2000/getmsg',{ headers: { 'Authorizan': token } })
+    }
+    else {
+        axios.get(`http://localhost:2000/getmsg?lastmsgId=${lastmsgId}`, { headers: { 'Authorizan': token } })
             .then(res => {
-                createUlList()
-                res.data.forEach((el)=>{
-                 showMsgOnScreen(el.senderName, el.message)
+                if (lastmsgId == -1) {
+                    createUlList()
+                }
+                res.data.forEach((el) => {
+                    bkcontainer.push(el);
+                    if(bkcontainer.length>10){
+                        bkcontainer.shift();
+                    }
+                    lastmsgId = el.messageId
+                    showMsgOnScreen(el.senderName, el.message)
+                    localStorage.setItem('storeMsg', JSON.stringify(bkcontainer))
+
                 })
             })
             .catch(err => console.log(err))
@@ -22,7 +50,7 @@ msgInput.addEventListener('submit', async (event) => {
     try {
         const post = await axios.post(`http://localhost:2000/msgpost`, { message: message }, { headers: { 'Authorizan': token } })
         const getmessage = post.data.message;
-    //    showMsgOnScreen("You", getmessage)
+        //    showMsgOnScreen("You", getmessage)
         document.getElementById('sendmsg').value = '';
     }
     catch (err) {
@@ -39,25 +67,33 @@ function showMsgOnScreen(senderName, message) {
 
 //for log out
 
-document.getElementById('lgout').addEventListener('click',()=>{
+document.getElementById('lgout').addEventListener('click', () => {
     localStorage.removeItem('token')
-    window.location.href='/login.html';
+    window.location.href = '/login.html';
 })
 
-setInterval(async()=>{
-    try{
-   const allmsg = await axios.get('http://localhost:2000/getmsg',{ headers: { 'Authorizan': token } })
-         createUlList();
-       allmsg.data.forEach((el)=>{
-        showMsgOnScreen(el.senderName, el.message)
-    })
-}
-    catch(err){
+setInterval(async () => {
+    try {
+        const allmsg = await axios.get(`/getmsg?lastmsgId=${lastmsgId}`, { headers: { 'Authorizan': token } })
+        // createUlList();
+        allmsg.data.forEach((el) => {
+            lastmsgId = el.messageId;
+            bkcontainer.push(el)
+            if(bkcontainer.length>10){
+                bkcontainer.shift();
+            }
+            localStorage.setItem('storeMsg', JSON.stringify(bkcontainer))
+            console.log(bkcontainer)
+            console.log('lasmst id',lastmsgId)
+            showMsgOnScreen(el.senderName, el.message)
+        })
+    }
+    catch (err) {
         console.log(err)
     }
-},1000)
-function createUlList(){
-    const divlist=document.getElementById("msgCont");
-  const ullis=` <ul class="licon" id="ulitem"></ul>`
-  divlist.innerHTML=ullis;
+}, 1000)
+function createUlList() {
+    const divlist = document.getElementById("msgCont");
+    const ullis = ` <ul class="licon" id="ulitem"></ul>`
+    divlist.innerHTML = ullis;
 }
